6d6b360343f314e5c0240677426ba37b729369a0
Van Nong <nodochau@yahoo.com>
2022-07-07 14:13:17 -0400

    Second commit

--- lib/Tasks/tasks_title.dart
+++ lib/Tasks/tasks_title.dart
@@ -16,8 +16,20 @@ class TaskTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //final item = Provider.of<TaskData>(context).getIndex(taskTitle);
     return Dismissible(
+        key: Key(taskTitle),
+        onDismissed: (direction) {
+          Provider.of<TaskData>(context, listen: false).deleteTask(taskTitle);
+        },
+        background: Container(
+          color: const Color.fromARGB(255, 198, 19, 7),
+          child: const Center(
+            child: Text(
+              'Deleting....Deleting...Deleting',
+              style: TextStyle(color: Colors.white, fontSize: 15.0),
+            ),
+          ),
+        ),
         child: ListTile(
           // onLongPress: () {
           //   Provider.of<TaskData>(context, listen: false).deleteTask(taskTitle);
@@ -33,10 +45,6 @@ class TaskTile extends StatelessWidget {
             value: isChecked,
             onChanged: checkBoxcallBack,
           ),
-        ),
-        key: Key(taskTitle),
-        onDismissed: (direction) {
-          Provider.of<TaskData>(context, listen: false).deleteTask(taskTitle);
-        });
+        ));
   }
 }
